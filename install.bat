94a909d7baa9cb9282066aaae4e9d4e8bfbf974e
Stav Sapir <stavsap@gmail.com>
2024-03-11 23:13:20 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,13 @@
 @echo off
 
-REM Downloading the repository
 git clone https://github.com/comfyanonymous/ComfyUI.git
 
 cd ComfyUI
 python -m venv venv
 call venv\Scripts\activate.bat
+
+python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
+
 python main.py --windows-standalone-build --listen
