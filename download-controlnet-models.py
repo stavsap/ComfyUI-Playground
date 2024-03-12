3542225212781c854e39dcf2f20c5dbfcd13c9dd
Stav Sapir <stavsap@gmail.com>
2024-03-12 18:06:54 +0200

    Update download-controlnet-models.py

--- download-controlnet-models.py
+++ download-controlnet-models.py
@@ -1,29 +1,51 @@
-from huggingface_hub import hf_hub_download
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11e_sd15_ip2p.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11e_sd15_ip2p.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11e_sd15_shuffle.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11e_sd15_shuffle.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11f1e_sd15_tile.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11f1e_sd15_tile.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11f1p_sd15_depth.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11f1p_sd15_depth.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_canny.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_canny.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_inpaint.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_inpaint.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_lineart.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_lineart.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_mlsd.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_mlsd.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_normalbae.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_normalbae.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_openpose.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_openpose.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_scribble.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_scribble.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_seg.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_seg.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_softedge.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15_softedge.yaml", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15s2_lineart_anime.pth", local_dir=".")
-hf_hub_download(repo_id="lllyasviel/ControlNet-v1-1", filename="control_v11p_sd15s2_lineart_anime.yaml", local_dir=".")
+import requests
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+downloads = [
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11e_sd15_ip2p.pth","control_v11e_sd15_ip2p.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11e_sd15_ip2p.yaml","control_v11e_sd15_ip2p.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11e_sd15_shuffle.pth","control_v11e_sd15_shuffle.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11e_sd15_shuffle.yaml","control_v11e_sd15_shuffle.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth","control_v11f1e_sd15_tile.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.yaml","control_v11f1e_sd15_tile.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth","control_v11f1p_sd15_depth.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.yaml","control_v11f1p_sd15_depth.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth","control_v11p_sd15_canny.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.yaml","control_v11p_sd15_canny.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint.pth","control_v11p_sd15_inpaint.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_inpaint.yaml","control_v11p_sd15_inpaint.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart.pth","control_v11p_sd15_lineart.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_lineart.yaml","control_v11p_sd15_lineart.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_mlsd.pth","control_v11p_sd15_mlsd.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_mlsd.yaml","control_v11p_sd15_mlsd.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_normalbae.pth","control_v11p_sd15_normalbae.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_normalbae.yaml","control_v11p_sd15_normalbae.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.pth","control_v11p_sd15_openpose.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_openpose.yaml","control_v11p_sd15_openpose.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_scribble.pth","control_v11p_sd15_scribble.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_scribble.yaml","control_v11p_sd15_scribble.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_seg.pth","control_v11p_sd15_seg.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_seg.yaml","control_v11p_sd15_seg.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_softedge.pth","control_v11p_sd15_softedge.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_softedge.yaml","control_v11p_sd15_softedge.yaml","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15s2_lineart_anime.pth","control_v11p_sd15s2_lineart_anime.pth","./models/controlnet")
+        ("https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15s2_lineart_anime.yaml","control_v11p_sd15s2_lineart_anime.yaml","./models/controlnet")
+]
+
+def download_file(url, file_name, path):
+    with requests.get(url, stream=True, allow_redirects=True) as response:
+        total_size = int(response.headers.get('content-length', 0))
+        block_size = 4096  # 4KB blocks
+        progress_bar = tqdm(total=total_size, unit='B', unit_scale=True, desc=file_name)
+        with open(path+file_name, 'wb') as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+
+        progress_bar.close()
+
+with ThreadPoolExecutor(max_workers=4) as executor:
+    future_to_download = {executor.submit(download_file, url, file_name, path): file_name for url, file_name, path in downloads}
+    for future in as_completed(future_to_download):
+        file_name = future_to_download[future]
