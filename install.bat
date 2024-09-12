13fce9eb2152d6ee555081476c6368c2feaf6aee
Stav Sapir <stavsap@gmail.com>
2024-09-12 21:19:20 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -10,7 +10,7 @@ call venv\Scripts\activate.bat
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 
-pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu121
+pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu124
 
 python -m pip install huggingface_hub opencv-python insightface onnxruntime onnxruntime-gpu simpleeval
 
